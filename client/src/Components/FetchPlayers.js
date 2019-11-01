89ea6fed0f6a28e36642e12a7bff9897ee846750
theolamide <ajadidan@gmail.com>
2019-11-01 11:15:23 -0400

    Fetach Players component setup

---
+++ client/src/Components/FetchPlayers.js
@@ -0,0 +1,19 @@
+import React from "react";
+import axios from 'axios';
+
+export default class FetchPlayers extends React.Component{
+
+
+
+
+
+
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
